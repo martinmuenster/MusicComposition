165923d12c73c465bd6c40b6a78a343e5898a4cc
Martin Muenster <mm617@duke.edu>
2018-04-18 12:34:56 -0400

    wrote mp3 files

--- csvWriter.py
+++ csvWriter.py
@@ -18,10 +18,19 @@ def openFiles():
 
 
 def CSVlist(csvfile, index):
-    note_rows = []
+    note_rows = [0]*8
     reader = csv.reader(csvfile)
     time = 0;
     prev_row = ['0'] * 128
+    note_rows[0] = [0,0,' Header',1,7,120]
+    note_rows[1] = [1, 0, ' Start_track']
+    note_rows[2] = [1, 0, ' Title_t', '\"wirugbwr\"']
+    note_rows[3] = [1, 0, ' Time_signature', 3, 2, 24, 8]
+    note_rows[4] = [1, 0, ' Tempo', 375000]
+    note_rows[5] = [1, 0, ' End_track']
+    note_rows[6] = [2, 0, ' Start_track']
+    note_rows[7] = [2, 0, ' Title_t', '"Piano"']
+
     for row in reader:
 
 
@@ -31,17 +40,21 @@ def CSVlist(csvfile, index):
         for velocity in row:
             if velocity != prev_row[pitch]:
                 note_row = [0] * 6
-                note_row[0] = 1
+                note_row[0] = 2
                 note_row[2] = ' Note_on_c'
                 note_row[3] = 0
                 note_row[1] = time
                 note_row[4] = pitch
-                note_row[5] = velocity
+                if velocity == '1':
+                    note_row[5] = 80
+                else:
+                    note_row[5] = 0
                 note_rows.append(note_row)
             pitch += 1
         time += 30
         prev_row = row
-
+    note_rows.append([2, time, ' End_track'])
+    note_rows.append([0, 0, ' End_of_file'])
 
         # aIndex = 0
         # bIndex = 0
